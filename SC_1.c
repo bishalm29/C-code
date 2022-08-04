a643a8864182dbfebc0e06e52ba62489081bf185
Bishal <bishalmaity29@gmail.com>
2022-08-04 14:48:39 +0530

    Add files via upload

---
+++ SC_1.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+include<math.h>
+
+double f(double e,double T,double del)
+{
+    double f;
+    f=tanh(0.5*sqrt(e*e+del*del)/T)/(sqrt(e*e+del*del));
+    return f;
+}
+int main()
+{
+    int i;
+    double s;
+ff
+
+}
